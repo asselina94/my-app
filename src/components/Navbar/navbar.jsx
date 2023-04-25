3a721c4cba53125b36763a09c2af42db20f54a88
Assel Batu <batuassel@gmail.com>
2023-04-25 11:36:21 +0200

    search icon

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,21 +1,24 @@
 
 import { ReactComponent as SearchlongIcon } from "assets/Logo_icon.svg";
-//import { ReactComponent as SearchnobackIcon } from "assets/longNoback.svg";
+import { ReactComponent as SearchIcon } from "assets/search_icon.svg";
 //import { ReactComponent as SearchmidIcon } from "assets/searchMid.svg";
 //import { ReactComponent as SearchshortIcon } from "assets/searchShort.svg";
 import "./Navbar.css"
 
 const Navbar = () => {
   return (
-    <div className = "search">
+    <nav>
       <div className = "wrapper">
         <SearchlongIcon />
         <div class = "textWrapper">
           <p className="music">ZaMusic</p>
         </div>
+        <div className="search">
+        <SearchIcon /></div>
       </div>  
-    </div>
-  )
+      
+    </nav>
+  );
 }
 
 export default Navbar
